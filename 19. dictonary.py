8e0f08ba33847b86828ec7b346f2ecab8bf15c22
yashwanth192411132 <192411132.simats@saveetha.com>
2025-06-22 10:54:47 +0530

    Create 19. dictonary.py

---
+++ 19. dictonary.py
@@ -0,0 +1,12 @@
+# Sample dictionary
+my_dict = {
+    'name': 'Alice',
+    'age': 25,
+    'city': 'New York'
+}
+
+# Find the length of the dictionary
+length = len(my_dict)
+
+# Display the result
+print("Length of the dictionary:", length)
