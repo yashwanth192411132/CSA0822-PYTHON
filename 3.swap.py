cd81b8e07d5bc060b631894375cbb634ee854ab4
yashwanth192411132 <192411132.simats@saveetha.com>
2025-06-22 10:36:31 +0530

    Create 3.swap.py

---
+++ 3.swap.py
@@ -0,0 +1,13 @@
+# Swapping the values of two variables
+
+# Input from the user
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+
+# Swapping
+a, b = b, a
+
+# Output the result
+print("After swapping:")
+print("a =", a)
+print("b =", b)
