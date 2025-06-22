6c462e3023ccf3d59ad73c6d66fa7bb1a4adb1c5
yashwanth192411132 <192411132.simats@saveetha.com>
2025-06-22 10:37:15 +0530

    Create 4.arethamatic.py

---
+++ 4.arethamatic.py
@@ -0,0 +1,11 @@
+#arithmetic operator
+num1=int(input("enter num1"))
+num2=int(input("enter num2"))
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+print("addition=",addition)
+print("subtraction=",subtraction)
+print("multiplication=",multiplication)
+print("division=",division)
